f67b1c329e9305c95f76692b6a412c5e2bb21f8f
renantescaro <renantescaro@hotmail.com>
2022-04-25 21:55:48 -0300

    adicionando função para salvar imagem

--- run.py
+++ run.py
@@ -4,34 +4,34 @@ from PIL import Image
 from binarizacao import Binarizacao
 
 
-imagem = cv2.imread('assets/original.jpg')
+def salvar_imagem(caminho, imagem):
+    plt.imsave(caminho, imagem)
+    plt.imshow(imagem)
+    plt.show()
+
+
+resolucao_corte = (545, 180)
+caminho_imagens = 'assets/carros/'
+imagem = cv2.imread(f'{caminho_imagens}original.jpg')
 imagem = cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB)
 
 # redimensionamento
-imagem = cv2.resize(imagem, (600, 195))
-plt.imsave('assets/0.png', imagem)
-plt.imshow(imagem)
-plt.show()
+imagem = cv2.resize(imagem, resolucao_corte)
+salvar_imagem(f'{caminho_imagens}/0.png', imagem)
 
 # binarização
 imagem = Binarizacao(
     imagem=Image.fromarray(imagem),
     limiar=93
 ).processar()
-plt.imsave('assets/1.png', imagem)
-plt.imshow(imagem)
-plt.show()
+salvar_imagem(f'{caminho_imagens}/1.png', imagem)
 
 # erosão
 rect = cv2.getStructuringElement(cv2.MORPH_RECT, (8, 8))
 imagem = cv2.erode(imagem, rect, iterations=1)
-plt.imsave('assets/2.png', imagem)
-plt.imshow(imagem)
-plt.show()
+salvar_imagem(f'{caminho_imagens}/2.png', imagem)
 
 # dilatação
 elip = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
 imagem = cv2.dilate(imagem, elip, iterations=2)
-plt.imsave('assets/3.png', imagem)
-plt.imshow(imagem)
-plt.show()
+salvar_imagem(f'{caminho_imagens}/3.png', imagem)
